321bf2bcd39401f1af547c74215c5368c435e433
Joan in DC <joanrigdon@gmail.com>
2018-12-30 02:29:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   pigeons_list = {}
 
@@ -9,13 +11,14 @@ def nyc_pigeon_organizer(data)
   data.each do |attrib_key, attrib_hash|
     attrib_hash.each do |attrib_val, array|
       array.each do |pigeon|
-        pigeons_list[pigeon] = {}
+        pigeons_list[pigeon] ||= {}
         pigeons_list[pigeon][attrib_key] ||= []
         pigeons_list[pigeon][attrib_key] << attrib_val.to_s
       end    
     end
   end
   pigeons_list
+  binding.pry
 end
 
   
