5fd91f717bcc5203989affc44d5dcc5c6a13b10c
Joan in DC <joanrigdon@gmail.com>
2018-12-30 02:12:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,14 +17,12 @@ def nyc_pigeon_organizer(data)
   #gives each pigeon 3 attrib_keys, color, gender, lives
         #if !pigeons_list.has_key?(attrib_key)
           pigeons_list[pigeon][attrib_key] ||= []
-        end
         
   #tries to populate each attrib key with attrib values, entering values as strings .. BUT -- ? -- if pigeon has 2 colors, only one is captured.
   
         pigeons_list[pigeon][attrib_key] << attrib_val.to_s
         
           
-      end
     end
   end
   pigeons_list
