73f44ed36cd78ed01b1e305ad69e3af9dfc7e725
Joan in DC <joanrigdon@gmail.com>
2018-12-30 02:12:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,13 +11,12 @@ def nyc_pigeon_organizer(data)
       array.each do |pigeon|
       
   #gives new hash 7 keys, each with pigeon name    
-        if !pigeons_list.has_key?(pigeon)
-          pigeons_list[pigeon] = {}
-        end
+        #if !pigeons_list.has_key?(pigeon)
+          pigeons_list[pigeon] ||= {}
         
   #gives each pigeon 3 attrib_keys, color, gender, lives
-        if !pigeons_list.has_key?(attrib_key)
-          pigeons_list[pigeon][attrib_key] = []
+        #if !pigeons_list.has_key?(attrib_key)
+          pigeons_list[pigeon][attrib_key] ||= []
         end
         
   #tries to populate each attrib key with attrib values, entering values as strings .. BUT -- ? -- if pigeon has 2 colors, only one is captured.
