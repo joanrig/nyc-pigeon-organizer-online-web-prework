2f7ccf099190432cf37ff1d58ca41516c081bb46
Joan in DC <joanrigdon@gmail.com>
2018-12-30 01:26:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require "pry"
 def nyc_pigeon_organizer(data)
   pigeons_list = {}
 
-  #attrib_key = color or gender or lives
+  #attrib_key = label: color, gender or lives
   #attrib_val = ie: purple or male or subway
   #array = array of pigeon names
   #name = pigeon name
@@ -14,20 +14,11 @@ def nyc_pigeon_organizer(data)
       
   #gives new hash 7 keys, each with pigeon name    
         if !pigeons_list.has_key?(pigeon)
-          pigeons_list[pigeon] = {}
-        end
-        
-  #gives each pigeon 3 attrib_keys, color, gender, lives
-        if !pigeons_list.has_key?(attrib_key)
-          pigeons_list[pigeon][attrib_key] = []
-        end
-        
-  #tries to populate each attrib key with attrib values, entering values as strings .. BUT -- ? -- if pigeon has 2 colors, only one is captured.
-  
-        if !pigeons_list.has_val?(attrib_val)
-          pigeons_list[pigeon][attrib_key] << attrib_val.to_s
-        else attrib_key.push(attrib_val)
+          pigeons_list[pigeon] = {} 
           
+  #adds attrib_ keys & vals to existing pigeons, but still captures only one color per pigeon.
+        else 
+          pigeons_list[pigeon][attrib_key] = attrib_val.to_s
         end
       end
     end
